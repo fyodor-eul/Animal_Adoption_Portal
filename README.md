41d3afd7e1a8803e53f9331305f695638f1a5854
Thiha Htoo Zin (Fyodor) <thihahtoozinn@gmail.com>
2026-01-06 13:12:25 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Animation Adoption - CRUD Project
+
+
---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function openTab(name){
+    /*
+    This funciton handle what happens if we click on each tab
+    including style changes and calling functions
+    */
+
+    /* Log */
+    console.log(name);
+
+    /* Remove all active highlights on tabs */
+    var tabs = document.getElementsByClassName('tab');
+    for(i = 0; i < tabs.length; i++){
+        tabs[i].classList.remove('active');
+    }
+    /* Highlight the current tab */
+    if(name === "home"){
+        document.getElementById("homeTab").classList.add('active');
+    }else if(name == "gallery"){
+        document.getElementById("galleryTab").classList.add('active');
+    }else if(name == "about"){
+        document.getElementById("aboutTab").classList.add('active');
+    }else{
+        console.log("error");
+    }
+}
