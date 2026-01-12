0b060f732bc7b0b407d71008a6c7259593ffabf4
Thiha Htoo Zin (Fyodor) <thihahtoozinn@gmail.com>
2026-01-12 22:49:56 +0800

    implement server side js with node and express

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express")
+var app = express()
+
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.route('/index.html').get(function(req,res){
+    //
+});
+
+
+app.listen(1338, "127.0.0.1");
+console.log("[+] Web server is running @ http://127.0.0.1:1338");
